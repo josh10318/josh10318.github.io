8c04aed8b980f9d64d2037a34fa29b1f6500375e
josh10318 <joshua.alridge@carvercollegiate.org>
2025-09-29 14:19:37 +0000

    platformer

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,22 +27,39 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    toggleGrid();
 
 
     // TODO 2 - Create Platforms
-createPlatform
-
-
-
+       createPlatform(100,700,200,15,"black");
+       createPlatform(300,600,200,15,"black");
+       createPlatform(500,500,200,15,"black");
+       createPlatform(300,400,200,15,"black");
+       createPlatform(100,300,200,15,"black");
+       createPlatform(300,200,200,15,"black");
+       createPlatform(700,400,200,15,"black");
+       createPlatform(900,300,200,15,"black");
+       createPlatform(1100,200,200,15,"black");
+       createPlatform(700,600,200,15,"black");
+       createPlatform(1100,600,200,15,"black");
+       
     // TODO 3 - Create Collectables
-
-
-
-    
+       createCollectable("database", 900, 100, 0.7, 0.5);
+       createCollectable("database", 700, 200, 0.7, 0.5);
+       createCollectable("database", 300, 500, 0.7, 0.5);
+       createCollectable("database", 1100, 500, 0.7, 0.5);
+       createCollectable("database", 700, 500, 0.7, 0.5);
+       createCollectable("database", 1200, 100, 0.7, 0.5);
+       createCollectable("database", 200, 100, 0.7, 0.5);
+       createCollectable("database", 300, 100, 0.7, 0.5);
+       createCollectable("database", 500, 400, 0.7, 0.5);
+       
     // TODO 4 - Create Cannons
-
-
+       createCannon("right", 300, 1300)
+        createCannon("right", 600, 1300)
+         createCannon("top", 300, 1300)
+         createCannon("top", 600, 1300)
+         
     
     
     //////////////////////////////////
